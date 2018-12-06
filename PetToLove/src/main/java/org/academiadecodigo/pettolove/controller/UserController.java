7fc641fe5fae7ed3ce6ebe69b941e9b7a5cc1575
joaoenes <joaofenes@gmail.com>
2018-12-06 17:52:33 -0100

    add and edit User

--- PetToLove/src/main/java/org/academiadecodigo/pettolove/controller/UserController.java
+++ PetToLove/src/main/java/org/academiadecodigo/pettolove/controller/UserController.java
@@ -1,12 +1,20 @@
 package org.academiadecodigo.pettolove.controller;
 
+import org.academiadecodigo.pettolove.command.UserDTO;
 import org.academiadecodigo.pettolove.converters.UserDTOToUser;
 import org.academiadecodigo.pettolove.converters.UserToUserDTO;
+import org.academiadecodigo.pettolove.persistence.model.User;
 import org.academiadecodigo.pettolove.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -33,7 +41,7 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/{id}")
-    public ResponseEntity<UserDto> showCustomer(@PathVariable Integer id) {
+    public ResponseEntity<UserDTO> showUser(@PathVariable Integer id) {
 
         User user = userService.get(id);
 
@@ -41,6 +49,46 @@ public class UserController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
-        return new ResponseEntity<>(userToUserDto.convert(user), HttpStatus.OK);
+        return new ResponseEntity<>(userToUserDTO.convert(user), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = {"/", ""})
+    public ResponseEntity<?> addUser(@Valid @RequestBody UserDTO userDto, BindingResult bindingResult, UriComponentsBuilder uriComponentsBuilder) {
+
+        if (bindingResult.hasErrors() || userDto.getId() != null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        User savedUser = userDto.save(userDOToUser.convert(userDto));
+
+        // get help from the framework building the path for the newly created resource
+        UriComponents uriComponents = uriComponentsBuilder.path("/api/customer/" + saveduser.getId()).build();
+
+        // set headers with the created path
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uriComponents.toUri());
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/{id}")
+    public ResponseEntity<UserDTO> editUser(@Valid @RequestBody UserDTO userDto, BindingResult bindingResult, @PathVariable Integer id) {
+
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (userDto.getId() != null && !userDto.getId().equals(id)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (userService.get(id) == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        userDto.setId(id);
+
+        userService.save(userDTOToUser.convert(userDto));
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
