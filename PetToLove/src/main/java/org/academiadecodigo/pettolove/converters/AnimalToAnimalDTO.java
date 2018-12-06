816f41de8abc45122519ce18d0025a733b9f8f34
andremelo <andre.medeiros.melo@gmail.com>
2018-12-06 17:10:01 -0100

    Adding AbstratConverter, Methods of all Converters done

--- PetToLove/src/main/java/org/academiadecodigo/pettolove/converters/AnimalToAnimalDTO.java
+++ PetToLove/src/main/java/org/academiadecodigo/pettolove/converters/AnimalToAnimalDTO.java
@@ -1,4 +1,25 @@
 package org.academiadecodigo.pettolove.converters;
 
-public class AnimalToAnimalDTO {
+import org.academiadecodigo.pettolove.command.AnimalDTO;
+import org.academiadecodigo.pettolove.persistence.model.Animal;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class AnimalToAnimalDTO extends AbstractConverters<Animal, AnimalDTO> {
+
+    @Override
+    public AnimalDTO convert(Animal animal) {
+
+        AnimalDTO animalDTO = new AnimalDTO();
+
+        animalDTO.setId(animal.getId());
+        animalDTO.setName(animal.getName());
+        animalDTO.setUserId(animal.getUserId());
+        animalDTO.setRaca(animal.getRaca());
+        animalDTO.setAnimalType(animal.getAnimalType());
+        animalDTO.setDescription(animal.getDescription());
+
+        return animalDTO;
+    }
 }
