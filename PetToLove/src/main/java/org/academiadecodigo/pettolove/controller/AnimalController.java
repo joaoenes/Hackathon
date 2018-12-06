ef80a8d72b3b4afb352d7c6ea1b335e6f086a889
joaoenes <joaofenes@gmail.com>
2018-12-06 16:57:24 -0100

    minor alterations

--- PetToLove/src/main/java/org/academiadecodigo/pettolove/controller/AnimalController.java
+++ PetToLove/src/main/java/org/academiadecodigo/pettolove/controller/AnimalController.java
@@ -1,4 +1,15 @@
 package org.academiadecodigo.pettolove.controller;
 
+import org.academiadecodigo.pettolove.services.AnimalService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/animal")
 public class AnimalController {
+
+    private AnimalService animalService;
+
 }
--- PetToLove/src/main/java/org/academiadecodigo/pettolove/controller/UserController.java
+++ PetToLove/src/main/java/org/academiadecodigo/pettolove/controller/UserController.java
@@ -1,4 +1,36 @@
 package org.academiadecodigo.pettolove.controller;
 
+import org.academiadecodigo.pettolove.converters.UserDTOToUser;
+import org.academiadecodigo.pettolove.converters.UserToUserDTO;
+import org.academiadecodigo.pettolove.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private UserService userService;
+    private UserToUserDTO userToUserDTO;
+    private UserDTOToUser userDTOToUser;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    public void setUserDTOToUser(UserDTOToUser userDTOToUser) {
+        this.userDTOToUser = userDTOToUser;
+    }
+
+    @Autowired
+    public void setUserToUserDTO(UserToUserDTO userToUserDTO) {
+        this.userToUserDTO = userToUserDTO;
+    }
+
+    
 }
