8516cf319fb38f33fcf34928c49a31db3401e8b7
joaoenes <joaofenes@gmail.com>
2018-12-06 17:27:52 -0100

    Some changes to check dev

--- PetToLove/src/main/java/org/academiadecodigo/pettolove/controller/UserController.java
+++ PetToLove/src/main/java/org/academiadecodigo/pettolove/controller/UserController.java
@@ -4,9 +4,9 @@ import org.academiadecodigo.pettolove.converters.UserDTOToUser;
 import org.academiadecodigo.pettolove.converters.UserToUserDTO;
 import org.academiadecodigo.pettolove.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -32,5 +32,15 @@ public class UserController {
         this.userToUserDTO = userToUserDTO;
     }
 
-    
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public ResponseEntity<UserDto> showCustomer(@PathVariable Integer id) {
+
+        User user = userService.get(id);
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(userToUserDto.convert(user), HttpStatus.OK);
+    }
 }
