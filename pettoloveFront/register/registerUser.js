37d7ebe8ac7aa130f14d41bdf20859ae9852db46
joaoenes <joaofenes@gmail.com>
2018-12-07 12:21:26 -0100

    Added front end

---
+++ pettoloveFront/register/registerUser.js
@@ -0,0 +1,35 @@
+
+function addUser(){
+    var email = $('.email').val();
+    var username = $('.username').val();
+    var name = $('.name').val();
+    var password = $('.password').val();
+    console.log('Lets start');
+    $.ajax({
+        url: 'http://localhost:8080/pettolove/user/',
+        type: 'POST',
+        data: JSON.stringify({
+            id: '',
+            email: email,
+            name: name,
+            username: username,
+            password: password
+
+        }),
+
+        async: true,
+        contentType: 'application/json',
+        success:userAdded,
+        error: errorCallback
+    });
+    
+}
+
+function userAdded(){
+    console.log('jhbjhsbn');
+    window.location.assign("../homepage/index.html");
+}
+function errorCallback(){
+console.log('jkjhsn');
+    alert("Error.");
+}
