7d2899f6f2ad5f253e61ec1aaa2821a0540d7ffc
Ana Jesus <beaasilveiira@gmail.com>
2018-12-07 05:33:40 -0100

    Login

--- pettoloveFront/login/login.js
+++ pettoloveFront/login/login.js
@@ -1,5 +1,33 @@
 $(document).ready(function () {
 
     loginUser();
+
 });
 
+function loginUser() {
+
+
+    $.ajax({
+        url: 'http://localhost:8080/pettolove/user',
+        type: 'GET',
+        async: true,
+        success: function (response) {
+            var username = $('.username').val();
+            var password = $('.password').val();
+
+            response.forEach(function(user) {
+                if(user.username === username){
+                    if(user.password === password){
+                        window.location.assign("homepage/index.html");
+                    }
+                }
+                
+            });
+        },
+        error: errorCallback
+    });
+}
+
+function errorCallback(){
+    alert("Error.");
+}
